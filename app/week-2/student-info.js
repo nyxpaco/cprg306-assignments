d2ebda71f8d5a8dc100e888de6d610d14af20b95
marianicolepaco <mariaalexandranicole.paco@edu.sait.ca>
2024-09-24 17:39:28 -0600

    Week 2 Assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+const StudentInfo = () => {
+    return (
+      <div>
+        <h1>Maria Alexandra Nicole Paco</h1>
+        <p>
+          <Link href="https://github.com/nyxpaco/cprg306-assignments">My Github</Link>
+          </p>
+      </div>
+    );
+  };
+  export default StudentInfo; 
+  
